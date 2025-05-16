b0a845ddf20e04bc4c8f06ff86b7209ced6bbff9
Cheveux <gaspard.szuszkin@gmail.com>
2025-05-16 12:01:48 +0200

    adding anim gsap

--- js/scroll.js
+++ js/scroll.js
@@ -42,8 +42,8 @@ document.querySelectorAll('.header_logo_section a').forEach(logo => {
 document.querySelectorAll('.devis_button').forEach(button => {
     button.addEventListener('mouseenter', () => {
         gsap.to(button, {
-            backgroundPosition: "100% 0%", // Simule un remplissage de haut en bas
-            duration: 0.5, // Durée de l'animation
+            backgroundPosition: "100% 0%", 
+            duration: 0.3, // Durée de l'animation
             ease: "power1.inOut"
         });
     });
@@ -55,4 +55,86 @@ document.querySelectorAll('.devis_button').forEach(button => {
             ease: "power1.inOut"
         });
     });
+});
+
+
+/*Animation au scroll des éléments de la sections services*/
+
+
+
+    gsap.utils.toArray('.header_section-services h1').forEach(h1 => {
+        gsap.from(h1, {
+            scrollTrigger: {
+                trigger: h1,
+                end : "top -50%",
+                toggleActions: "play reverse play reverse"
+            },
+            opacity: 0,
+            y: -200,
+            x: -400,
+            duration: 1.2,
+            ease: "power3.out"
+        });
+    });
+    gsap.from(".services_1-text", {
+        scrollTrigger: {
+            trigger: ".services_1",
+            toggleActions: "play reverse play reverse"
+        },
+        opacity: 0.5,
+        x: -300,
+    
+       
+        delay: 1,
+        ease: "power3.out"
+    });
+
+    gsap.from(".services_1-img", {
+        scrollTrigger: {
+            trigger: ".services_1",
+            toggleActions: "play reverse play reverse"
+        
+        },
+        opacity: 0.5,
+        x: 200,
+        duration: 1,
+        ease: "power3.out"
+    });
+
+
+gsap.from(".header_section-services .services_2", {
+    scrollTrigger: {
+        trigger: ".services_2",
+        start: "top 120%", // quand le haut de .services_1 atteint 80% du viewport
+        toggleActions: "play reverse play reverse"
+    },
+    opacity: 0.5,
+    x: 200,
+    y: 100,
+    duration: 1.2,
+    ease: "power3.out"
+});
+
+gsap.from(".services_2-text", {
+    scrollTrigger: {
+        trigger: ".services_2",
+        toggleActions: "play reverse play reverse"
+    },
+    opacity: 0.5,
+    x: -200,
+   
+    duration: 1.5,
+    ease: "power3.out"
+});
+
+gsap.from(".services_2-img", {
+    scrollTrigger: {
+        trigger: ".services_2",
+        toggleActions: "play reverse play reverse"
+     
+    },
+    opacity: 0.5,
+    x: 200,
+    duration: 1,
+    ease: "power3.out"
 });
