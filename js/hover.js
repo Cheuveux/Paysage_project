312c60b6bceca149fd85f9603fe13037ce6b6cfd
Cheveux <gaspard.szuszkin@gmail.com>
2025-05-20 16:15:53 +0200

    edit suite à meeting

--- js/hover.js
+++ js/hover.js
@@ -5,7 +5,7 @@ document.querySelectorAll('.header_nav li').forEach(li => {
         gsap.to(li.querySelector('a'), {
             y: -8,
             fontWeight:"900",
-            fontSize:"1.6srem",
+            fontSize:"1.3rem",
             color: "#A8C686", // vert clair
             duration: 0.3,
             ease: "power2.out"
@@ -24,6 +24,37 @@ document.querySelectorAll('.header_nav li').forEach(li => {
 
 // Animation GSAP pour les boutons de devis
 document.querySelectorAll('.devis_button').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        gsap.to(button, {
+            backgroundPosition: "100% 0%", 
+            duration: 0.3, // Durée de l'animation
+            ease: "power1.inOut"
+        });
+         gsap.to(button, {
+            borderWidth: "4px",
+            borderColor: "#6C8350", // vert du background
+            duration: 0.3,
+            ease: "power1.inOut"
+        });
+    });
+
+    button.addEventListener('mouseleave', () => {
+        gsap.to(button, {
+            backgroundPosition: "0% 0%", // Retour à l'état initial
+            duration: 0.1,
+            ease: "power1.inOut"
+        });
+         gsap.to(button, {
+            borderWidth: "0px",
+            borderColor: "transparent",
+            duration: 0.1,
+            ease: "power1.inOut"
+        });
+    });
+});
+
+// Animation GSAP pour les boutons d'envoi'
+document.querySelectorAll('.envoyer').forEach(button => {
     button.addEventListener('mouseenter', () => {
         gsap.to(button, {
             backgroundPosition: "100% 0%", 
